86f238b5b3e592b615cf9458e1a58b76c3f1e6aa
rir001 <riveraldrv@gmail.com>
2022-06-26 16:19:25 -0400

    v2

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/sas', methods=['GET'])
+def sas():
+    return 'google.com'
+
 if __name__ == '__main__':
     app.run(debug=True)
 
--- prueba.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-for n in os.listdir("source"):
-
-    with open(f"source/{n}/horario.csv") as hor:
-        hor = hor.read()
-        print(hor)
