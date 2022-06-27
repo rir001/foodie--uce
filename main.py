338fe341d06c4ef8428e79f4925a2556ddcdad8f
rir001 <riveraldrv@gmail.com>
2022-06-26 22:10:06 -0400

    v2.2

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/sas', methods=['GET'])
-def sas():
-    return 'google.com'
+@app.route('/pruebasss', methods=['GET'])
+def preba():
+    return render_template('prueba.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
