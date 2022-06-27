13a9e02e75ab5cb6b7206859ebaf52ec6610b09a
rir001 <riveraldrv@gmail.com>
2022-06-26 22:11:54 -0400

    v2.3

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/foodieuce', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/pruebasss', methods=['GET'])
+@app.route('/foodieuce/pruebasss', methods=['GET'])
 def preba():
     return render_template('prueba.html')
 
