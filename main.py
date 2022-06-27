0fcf4fe825f36bb0f65ed9767d12f9472db9b57f
rir001 <riveraldrv@gmail.com>
2022-06-27 00:44:00 -0400

    a

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+
+
+
+
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/foodieuce', methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
-
-@app.route('/foodieuce/prueba', methods=['GET'])
-def preba():
-    return render_template('prueba.html')
+    return render_template('/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
